0009de2a30596ad155ed4b9920ef37e8fb0a84b4
Krishnaghartimagar <unknown420magar@gmail.com>
2025-12-29 15:29:53 +0545

    react

---
+++ module.js
@@ -0,0 +1,8 @@
+export let a=5;
+ function abc(){
+    return "hello"
+}
+export default abc;
+ export function myfunction(){
+console.log("this  is my function")
+ }
---
+++ module1.js
@@ -0,0 +1,4 @@
+export function myfunction(){
+    console.log("hello this is krishna");
+
+}
